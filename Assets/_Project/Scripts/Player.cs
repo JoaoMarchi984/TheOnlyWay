d68605e355bf6f7259c13cd63e018b84ee7059cd
JoaoMarchi984 <marchijoao17@gmail.com>
2025-10-24 22:10:29 -0300

    Atualização 2 24/10/25

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    [Tooltip("Units per second")]
     public float moveSpeed = 5f;
 
     Rigidbody2D rb;
+    Animator animator;
+    Vector2 lastMoveDir = Vector2.down; 
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        Vector2 v = new Vector2(x, y).normalized * moveSpeed;
+
+        if (x != 0) y = 0;
+
+        Vector2 v = new Vector2(x, y) * moveSpeed;
         rb.velocity = v;
+
+        if (animator != null)
+        {
+            animator.SetFloat("MoveX", x);
+            animator.SetFloat("MoveY", y);
+
+            bool isMoving = v.magnitude > 0.1f;
+            animator.SetBool("IsMoving", isMoving);
+
+            if (isMoving)
+                lastMoveDir = new Vector2(x, y);
+
+            animator.SetFloat("LastMoveX", lastMoveDir.x);
+            animator.SetFloat("LastMoveY", lastMoveDir.y);
+        }
     }
 }
